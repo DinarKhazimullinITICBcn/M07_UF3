1c414f4992b8f9a336a6d7a47b71bee313688563
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2024-01-24 19:00:33 +0100

    Afegit exercici 3

---
+++ REPAS/exercici3.py
@@ -0,0 +1,6 @@
+valor1 = float(input('Insereix valor numero 1'))
+valor2 = float(input('Insereix valor numero 2'))
+if valor1 > valor2:
+    print("El valor ", valor1 ," es mes gran que ", valor2)
+else:
+    print("El valor ", valor2 ," es mes gran que ", valor1)
