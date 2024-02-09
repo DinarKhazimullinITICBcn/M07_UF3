dc9003235bcab34931869e71ab877a762163370f
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2024-02-09 17:25:47 +0100

    Afegit update y afegit la trucada a main. Tocat creat.py

--- PRACTICA2/docker/Main.py
+++ PRACTICA2/docker/Main.py
@@ -3,11 +3,13 @@ import connection
 import create_table
 import create
 import read
+import update
 try :
     conn, conectar = connection.connect()
     create_table.consulta(conn, conectar)
-    create.crear(conectar)
+    create.crearCharacter(conn, conectar)
     read.buscar(conectar)
+    update.actualitzar(conn, conectar)
 except (Exception, psycopg2.Error) as error:
     print("Error", error)
 finally:
--- PRACTICA2/docker/create.py
+++ PRACTICA2/docker/create.py
@@ -1,4 +1,4 @@
-def crearCharacter(conectar) : 
+def crearCharacter(conn, conectar) : 
     sql = """
             SELECT character_id
             FROM character
@@ -6,6 +6,15 @@ def crearCharacter(conectar) :
             LIMIT 1;
         """
     conectar.execute(sql)
-    result = conectar.fetchall()
-    
-    sql = """INSERT INTO CHARACTER VALUES ({id}, 'Dinar', 20, 'S tier', 'USEC', 'Infantry')"""
+    result = conectar.fetchone()
+    if result :
+        id = result[0]
+    else:
+        id = None
+    if id is not None :
+        id += 1
+        sql = f"""INSERT INTO CHARACTER VALUES (%s, 'Dinar', 20, 'S tier', 'USEC', 'Infantry')"""
+        conectar.execute(sql, (id,))
+        conn.commit()
+    else :
+        print("Error afegint usuari")
